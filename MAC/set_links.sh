13f84b1442ec3cd49b7f487bae5b65e7be7a68b5
Andrei Miclaus <miclaus.andrei@gmail.com>
2017-12-24 14:25:30 +0100

    newest versions of terminal configuration and docker aliases

---
+++ MAC/set_links.sh
@@ -0,0 +1,4 @@
+#remember to use absolute paths as ln -s sets the absolute path 
+ln -s /Users/andreimiclaus/personal/utilities/MAC/profile ~/.profile
+ln -s /Users/andreimiclaus/personal/utilities/MAC/vimrc ~/.vimrc
+ln -s /Users/andreimiclaus/personal/utilities/docker/docker-alias ~/.docker-alias

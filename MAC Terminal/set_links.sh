f4360eb1364eff32998050d19ba3fd9c7c786a09
Andrei Miclaus <andrei.miclaus@flywire.com>
2019-09-27 14:16:00 +0300

    renames, added watch

---
+++ MAC Terminal/set_links.sh
@@ -0,0 +1,10 @@
+#remember to use absolute paths as ln -s sets the absolute path and escape any special characters or spaces
+ln -f -s /Users/andrei.miclaus\@flywire.com/code/utilities/MAC\ Terminal/bash_profile ~/.profile
+ln -f -s /Users/andrei.miclaus\@flywire.com/code/utilities/MAC\ Terminal/vimrc ~/.vimrc
+ln -f -s /Users/andrei.miclaus\@flywire.com/code/utilities/docker/docker-alias ~/.docker-alias
+ln -f -s /Users/andrei.miclaus\@flywire.com/code/utilities/MAC\ Terminal/bash_aliases ~/.bash_aliases
+
+# Don't forget to install powerline
+# Don't forget to install fonts and set them:
+# Preferences > Text > Non-ASCII Font to '12pt Meslo LG S Regular for Powerline' fixed the issue for me.
+
--- MAC/set_links.sh
+++
@@ -1,5 +0,0 @@
-#remember to use absolute paths as ln -s sets the absolute path 
-ln -s /Users/andreimiclaus/personal/utilities/MAC/profile ~/.profile
-ln -s /Users/andreimiclaus/personal/utilities/MAC/vimrc ~/.vimrc
-ln -s /Users/andreimiclaus/personal/utilities/docker/docker-alias ~/.docker-alias
-ln -s ~/personal/utilities/MAC/my-alias ~/.my-alias

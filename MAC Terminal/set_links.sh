6b271e062766977193d193351500f6d92572f7dc
Andrei Miclaus <andrei.miclaus@flywire.com>
2022-07-08 14:30:35 +0300

    git status color & updates to links

--- MAC Terminal/set_links.sh
+++ MAC Terminal/set_links.sh
@@ -1,8 +1,9 @@
 #remember to use absolute paths as ln -s sets the absolute path and escape any special characters or spaces
-ln -f -s /Users/andrei.miclaus\@flywire.com/code/utilities/MAC\ Terminal/bash_profile ~/.profile
-ln -f -s /Users/andrei.miclaus\@flywire.com/code/utilities/MAC\ Terminal/vimrc ~/.vimrc
-ln -f -s /Users/andrei.miclaus\@flywire.com/code/utilities/docker/docker-alias ~/.docker-alias
-ln -f -s /Users/andrei.miclaus\@flywire.com/code/utilities/MAC\ Terminal/bash_aliases ~/.bash_aliases
+ln -f -s /Users/Andrei.Miclaus/Code/personal/utilities/MAC\ Terminal/bash_profile ~/.profile
+ln -f -s /Users/Andrei.Miclaus/Code/personal/utilities/MAC\ Terminal/vimrc ~/.vimrc
+ln -f -s /Users/Andrei.Miclaus/Code/personal/utilities/MAC\ Terminal/zshrc ~/.zshrc
+ln -f -s /Users/Andrei.Miclaus/Code/personal/utilities/MAC\ Terminal/bash_aliases ~/.bash_aliases
+ln -f -s /Users/Andrei.Miclaus/Code/personal/utilities/docker/docker-alias ~/.docker-alias
 
 # Don't forget to install powerline
 # Don't forget to install fonts and set them:
